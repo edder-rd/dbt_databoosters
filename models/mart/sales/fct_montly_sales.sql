d7947f4415179c43c6ce10a062393661ecba5e27
EDDER ROSALES DIAZ <edderrosalesdiaz@gmail.com>
2026-09-13 06:47:41 +0000

    correccion de nombres en fact

--- models/mart/sales/fct_montly_sales.sql
+++ models/mart/sales/fct_montly_sales.sql
@@ -1,6 +1,6 @@
 {{ config(tags=["dash_sales_bi"]) }}
 
-select date, id_salesperson, id_carmake, totalsale, totalcommission
-from {{ ref("psg_montly_sales_make") }} as psgm
+SELECT Date, ID_SALESPERSON, ID_CARMAKE, TotalSale, TotalCommission
+FROM {{ ref("psg_montly_sales_make") }} as psgm
 left join {{ ref("dim_carmake") }} as dimcm on psgm.car_make = dimcm.car_make
 left join {{ ref("dim_salesperson") }} as dimsp on psgm.salesperson = dimsp.salesperson
